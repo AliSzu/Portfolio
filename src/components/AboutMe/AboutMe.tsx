d7d045ed7d7358caab9db074ac064ea694c665d6
Alicja Szulecka <73056579+AliSzu@users.noreply.github.com>
2023-06-01 13:43:45 +0200

    changed slider icon color

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -18,7 +18,7 @@ const AboutMe = forwardRef<HTMLDivElement, IAboutMe>((props: IAboutMe, ref) => {
   const [catImg, setCatImg] = useState<Cat>();
   const [containerRef, isVisible] = useElementOnScreen({
     root: null,
-    rootMargin: "100px",
+    rootMargin: "10px",
     threshold: 0.0,
   });
 
@@ -51,47 +51,47 @@ const AboutMe = forwardRef<HTMLDivElement, IAboutMe>((props: IAboutMe, ref) => {
   return (
     <div className={`container ${classes["container-aboutMe"]}`}>
       <div className={"wrapper"} ref={ref}>
-        <Title title={"About me"} class={classes["title-aboutMe"]} />
-        <div
-          ref={containerRef}
-          className={`${classes.card} ${isVisible && "fade_in"}`}
-        >
-          <div>
-            <span className={classes.text}>
-              I'm Alicja a dedicated <b>third-year Computer Science student</b>{" "}
-              with a strong focus on becoming a Frontend developer! I am also an
-              active member of the <b>programming scientific club, BYTE_IT</b>{" "}
-              where I continuously challenge myself and enhance my skills. With
-              a solid foundation in Computer Science, I aim to create engaging
-              and user-friendly web as well as desktop applications. Oh! and I
-              love everything cute!
-              <br />
-            </span>
-            <div className={classes["aboutme-actions"]}>
-              <Icon
-                image={githubIcon}
-                onClick={() => handleLinkClick("https://github.com/AliSzu")}
-              />
-              <Icon
-                image={linkedinIcon}
-                onClick={() =>
-                  handleLinkClick(
-                    "https://www.linkedin.com/in/alicja-szulecka-579427279/"
-                  )
-                }
-              />
-              <Icon image={catIcon} onClick={handleCatClick} />
+        <div ref={containerRef} className={`${isVisible && "fade_in"}`}>
+          <Title title={"About me"} class={classes["title-aboutMe"]} />
+          <div ref={containerRef} className={classes.card}>
+            <div>
+              <span className={classes.text}>
+                I'm Alicja a dedicated{" "}
+                <b>third-year Computer Science student</b> with a strong focus
+                on becoming a Frontend developer! I am also an active member of
+                the <b>programming scientific club, BYTE_IT</b> where I
+                continuously challenge myself and enhance my skills. With a
+                solid foundation in Computer Science, I aim to create engaging
+                and user-friendly web as well as desktop applications. Oh! and I
+                love everything cute!
+                <br />
+              </span>
+              <div className={classes["aboutme-actions"]}>
+                <Icon
+                  image={githubIcon}
+                  onClick={() => handleLinkClick("https://github.com/AliSzu")}
+                />
+                <Icon
+                  image={linkedinIcon}
+                  onClick={() =>
+                    handleLinkClick(
+                      "https://www.linkedin.com/in/alicja-szulecka-579427279/"
+                    )
+                  }
+                />
+                <Icon image={catIcon} onClick={handleCatClick} />
+              </div>
             </div>
-          </div>
-          <div className={classes.cat}>
-            <img
-              src={catImg ? catImg.imgSrc : ZumiIcon}
-              alt={catImg ? catImg.shortDesc : "Zumi"}
-              className={classes["img-cat"]}
-            />
-            <span className={classes["text-cat"]}>
-              That's my little ray of sunshine - Zumi!
-            </span>
+            <div className={classes.cat}>
+              <img
+                src={catImg ? catImg.imgSrc : ZumiIcon}
+                alt={catImg ? catImg.shortDesc : "Zumi"}
+                className={classes["img-cat"]}
+              />
+              <span className={classes["text-cat"]}>
+                That's my little ray of sunshine - Zumi!
+              </span>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -3,25 +3,38 @@ import classes from "./Contact.module.scss";
 import Title from "../common/Title/Title";
 import GlassCard from "../common/GlassCard/GlassCard";
 import ContactForm from "./ContactForm/ContactForm";
-import emailIcon from "../../assets/images/emailIcon.png"
-import linkedinIcon from "../../assets/images/linkedinIcon.png"
+import emailIcon from "../../assets/images/emailIcon.png";
+import linkedinIcon from "../../assets/images/linkedinIcon.png";
+import useElementOnScreen from "../../hooks/useOnScreen";
 
 interface IContact {}
 
 const Contact = forwardRef<HTMLDivElement, IContact>((props: IContact, ref) => {
+  const [containerRef, isVisible] = useElementOnScreen({
+    root: null,
+    rootMargin: "10px",
+    threshold: 0.0,
+  });
+
   return (
     <div className={`container ${classes["container-contact"]}`} ref={ref}>
       <div className={"wrapper"}>
-        <Title title={"Contacts"} />
-        <div
-          className={classes["form-wrapper"]}
-        >
-          <GlassCard>
-            <ContactForm />
-          </GlassCard>
-          <div className={classes['link-wrapper']}>
-            <div className={classes['contact-link']}><img src={emailIcon} className={classes.icon}/><span>alicjaszulecka0@gmail.com</span></div>
-            <div className={classes['contact-link']}><img src={linkedinIcon} className={classes.icon}/><span>https://www.linkedin.com/in/alicja-szulecka/</span></div>
+        <div ref={containerRef} className={`${isVisible && "fade_in"}`}>
+          <Title title={"Contacts"} />
+          <div className={classes["form-wrapper"]}>
+            <GlassCard>
+              <ContactForm />
+            </GlassCard>
+            <div className={classes["link-wrapper"]}>
+              <div className={classes["contact-link"]}>
+                <img src={emailIcon} className={classes.icon} />
+                <span>alicjaszulecka0@gmail.com</span>
+              </div>
+              <div className={classes["contact-link"]}>
+                <img src={linkedinIcon} className={classes.icon} />
+                <span>https://www.linkedin.com/in/alicja-szulecka/</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
